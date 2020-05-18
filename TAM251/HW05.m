c7a100d3a0eea613c3263cb85f86d38d6113d636
Zhekai Li <49395799+ZhekaiLi@users.noreply.github.com>
2020-05-18 09:00:17 +0800

    Update HW05.m

--- TAM251/HW05.m
+++ TAM251/HW05.m
@@ -3,88 +3,97 @@ close all;
 clc;
 
 %% 5.1
-L = 1664; % mm
+L = 1645; % mm
 r = 22; % mm
-G = 66; % GPa
+G = 69; % GPa
 tu = 55; % MPa
-FS = 3;
+FS = 2;
 
-G = G * 1000;
+L = L/1e3; r = r/1e3; G = G*1e9; tu = tu*1e6; 
+
 tbyj = tu/r;
 vpa(tbyj * L / G / FS);
 
 %% 5.2
-L = 1766; % mm
-d = 28; % mm
-kt = 3; % kN.m
-T = 1294; % N.m
+L = 1697; % mm
+d = 34; % mm
+kt = 1; % kN.m
+T = 1950; % N.m
 
-vpa(T * d / 2 / kt / L);
+L = L/1e3; d = d/1e3; kt = kt*1e3;
+
+vpa(T * d/2 / kt / L * 1e3);
 
 %% 5.3
-L = 1787; % mm
-di = 19; % mm
-do = 33; % mm
-G = 33; % GPa
-T = 1918; % N.m
+L = 1673; % mm
+di = 14; % mm
+do = 30; % mm
+G = 32; % GPa
+T = 1961; % N.m
 
-do = do/1000;
-di = di/1000;
-G = G * 10^9;
+L = L/1e3; do = do/1e3; di = di/1e3; G = G*1e9;
 
 J = pi / 32 * (do^4 - di^4);
-vpa(T * di/2 / J / G * 1000);
+vpa(T * di/2 / J / G * 1e3);
 
 %% 5.4
-L1 = 374; % mm
-L2 = 358; % mm
-d1 = 80; % mm
-d2 = 45; % mm
-G1 = 35; % GPa
-G2 = 44; % GPa
-TB = 864; % N.m
-TC = 754; % N.m
+L1 = 301; % mm
+L2 = 373; % mm
+d1 = 67; % mm
+d2 = 50; % mm
+G1 = 30; % GPa
+G2 = 34; % GPa
+TB = 624; % N.m
+TC = 892; % N.m
+
+L1 = L1/1e3; L2 = L2/1e3; d1 = d1/1e3; d2 = d2/1e3; G1 = G1*1e9; G2 = G2*1e9;
 
 j1 = pi * d1^4 / 32;
 j2 = pi * d2^4 / 32;
 
 
-db = (TB-TC) * L1 / G1 / j1;
+db = (TC - TB) * L1 / G1 / j1;
 dc = (TC) * L2 / G2 / j2;
-vpa(dc - db);
+vpa(dc + db);
 
 %% 5.5
-L1 = 311; % mm
-L2 = 330; % mm
-d1 = 69; % mm
+L1 = 393; % mm
+L2 = 347; % mm
+d1 = 72; % mm
 d2 = 46; % mm
-G1 = 32; % GPa
-G2 = 33; % GPa
-TB = 626; % N.m
-TC = 926; % N.m
+G1 = 35; % GPa
+G2 = 32; % GPa
+TB = 930; % N.m
+TC = 933; % N.m
+
+L1 = L1/1e3; L2 = L2/1e3; d1 = d1/1e3; d2 = d2/1e3; G1 = G1*1e9; G2 = G2*1e9;
 
 j1 = pi * d1^4 / 32;
 j2 = pi * d2^4 / 32;
 
-vpa(TC * d2/2 / j2 * 1000);
-vpa((TB - TC) * d1/2 / j1 * 1000);
+vpa(TC * d2/2 / j2 /1e6);
+vpa((TC - TB) * d1/2 / j1 /1e6);
 
 %% 5.6
-d = 73; % mm
-TB = 1108; % N.m
-TC = 552; % N.m
+d = 60; % mm
+TB = 1390; % N.m
+TC = 693; % N.m
+
+d = d/1e3;
 
 j1 = pi / 32 * (d^4 - (d/2)^4);
 j2 = pi / 32 * d^4;
 
-vpa(TC * d/2 / j2 * 1000);
-vpa((TB - TC) * d/2 / j1 * 1000);
+vpa(-TC * d/2 / j2 /1e6);
+vpa((TB - TC) * d/2 / j1 /1e6);
 
 %% 5.7
-Rb = 94; % mm
-Rc = 35; % mm
+Rb = 93; % mm
+Rc = 40; % mm
 ft = 0.000004; % (1/N.m)
-TD = 695; % N.m
+TD = 614; % N.m
+
+Rb = Rb/1e3; Rc = Rc/1e3;
 
 db = TD * (Rb/Rc) * ft;
 dc = db * Rb / Rc;
@@ -93,27 +102,32 @@ ddc = TD * ft;
 vpa(ddc + dc);
 
 %% 5.8
-Rb = 71; % mm
-Rc = 36; % mm
+Rb = 70; % mm
+Rc = 39; % mm
 d = 12; % mm
-TD = 49; % N.m
+TD = 43; % N.m
 
-vpa(TD * Rb/Rc * d/2 / (pi/32 * d^4) * 1000);
+Rb = Rb/1e3; Rc = Rc/1e3; d = d/1e3;
+
+vpa(TD * Rb/Rc * d/2 / (pi/32 * d^4) /1e6);
 
 %% 5.9
-a = 1231; % mm
-b = 1509; % mm
-d = 41; % mm
-G = 68; % GPa
-tall = 22; % MPa
+a = 1321; % mm
+b = 1720; % mm
+d = 31; % mm
+G = 60; % GPa
+tall = 20; % MPa
+
+a = a/1e3; b = b/1e3; d = d/1e3; G = G*1e9; tall = tall*1e6;
 
 syms P;
 T = P * a;
-eq = tall == T * d/2 / (pi/32 * d^4);
+J = pi/32 * d^4;
+eq = tall == T * d/2 / J;
 P = solve(eq, P);
 
-vpa(P)
-
+sita = P*a * b / (G * J);
+vpa([P, sita]);
 
 
 
